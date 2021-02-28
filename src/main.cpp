24619b365f7badafa884262e62a276159d2c3cd6
michalk2k04 <michalk2k04@gmail.com>
2021-02-28 21:18:30 +0100

    Delete files and start rewrite code

--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,12 @@
 #include <Arduino.h>
-#include <Config.h>
-#include <Console.h>
-#include <LittleFS.h>
-#include <WebServer.h>
-#include <FS.h>
+#include <ESP8266WiFi.h>
 
-Config config = Config();
-
-Console console = Console();
-
-WebServer webServer = WebServer();
-
-void listDir(const char * dirname) {
-  Serial.printf("Listing directory: %s\n", dirname);
-
-  Dir root = LittleFS.openDir(dirname);
-
-  while (root.next()) 
-  {
-    File file = root.openFile("r");
-    Serial.print("  FILE: ");
-    Serial.println(root.fileName());
-    file.close();
-  }
-}
-
-void setup() 
+void setup()
 {
 
-    Serial.begin(9600); 
-    
-    Serial.println(" ");
-    Serial.println(" ");
-    Serial.println(" ");
-
-    if (!LittleFS.begin()) {
-        Serial.println("LittleFS mount failed");
-        return;
-    }
-
-    Serial.println("==============================");
-    listDir("/");
-    Serial.println("==============================");
-    
-    config.begin();
-
-
-    Serial.println("==============================");
-
-    for (int i = 0; i < 24; i++)
-    {
-      Serial.print("Template0 [");
-      Serial.print(i);
-      Serial.print("][0] = ");
-      Serial.print(config.template0[i][0]);
-      Serial.print(" ["+String(i)+"][1]");
-      Serial.println(";");
-    }
-    
-
-    Serial.println("==============================");
-
-    console.begin(config); 
-    webServer.begin(config);
-    
 }
-    
-void loop() 
+
+void loop()
 {
-  delay(10); 
-    console.handleConsole(); 
+
 }
-
-
-
